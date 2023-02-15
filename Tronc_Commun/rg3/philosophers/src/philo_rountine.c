d87fad89a2e9662e15f42cce00e459a0ddf89a8a
Atroooo <lucas.compiegne1@gmail.com>
2023-02-15 23:36:43 +0100

    plan for philo

--- Tronc_Commun/rg3/philosophers/src/philo_rountine.c
+++ Tronc_Commun/rg3/philosophers/src/philo_rountine.c
@@ -16,9 +16,12 @@ void	*routine(void *philo)
 {
 	pthread_mutex_t	mutex;
 	
-	while(!philo->dead)
+	while(!philo->dead) //ou que tous les philosophes ont mangé
 	{
 		//action
+		//mange : l'un des voisins ne pourra pas manger parce que celui qui mange va lui emprunter sa fourchette : mange le temps en ms
+		//dors : la fourchette du philo peut être emprunter : dors le temps en ms
+		//pense	: la fourchette du philo peut être emprunter : pense le temps en ms
 	}
 	return (NULL);
 }
