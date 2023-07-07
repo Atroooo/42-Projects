4cf6c30e0ed2c2e9fd93dc80b91bbd5338b70527
Atroooo <lucas.compiegne1@gmail.com>
2023-07-07 23:47:57 +0200

    start CPP

---
+++ Tronc_Commun/rg4/CPP00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 23:32:45 by marvin            #+#    #+#             */
+/*   Updated: 2023/07/07 23:32:45 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 0;
+		while (argv[++i])
+		{
+			j = -1;
+			while (argv[i][++j])
+				std::cout << (char)std::toupper(argv[i][j]);
+			if (argv[i + 1])
+				std::cout << " ";
+		}
+	}
+	return (0);
+}
