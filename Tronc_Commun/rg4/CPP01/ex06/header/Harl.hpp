1af565c6d6b08e954433156f552e2c9bffad5f38
Lucas Compiegne <lcompieg@z4r4p1.42lyon.fr>
2023-09-07 17:53:06 +0200

    ex05 and 06 done

---
+++ Tronc_Commun/rg4/CPP01/ex06/header/Harl.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcompieg <lcompieg@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 16:27:50 by lcompieg          #+#    #+#             */
+/*   Updated: 2023/09/07 16:28:55 by lcompieg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl {
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    public:
+        void complain(std::string level);
+};
+
+#endif
---
+++ Tronc_Commun/rg4/CPP01/ex06/src/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcompieg <lcompieg@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 17:47:12 by lcompieg          #+#    #+#             */
+/*   Updated: 2023/09/07 17:47:21 by lcompieg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void) {
+    Harl harl;
+
+    harl.complain("debug");
+    harl.complain("info");
+    harl.complain("warning");
+    harl.complain("error");
+    harl.complain("wrong");
+    return (0);
+}
